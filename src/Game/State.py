2e06703a3ae727d45eb4bedeb8a78ad79ecfbab6
Mauricio Perdomo <ml.perdomo97@gmail.com>
2020-11-04 21:48:25 -0500

    Add State module

---
+++ src/Game/State.py
@@ -0,0 +1,26 @@
+from collections import namedtuple
+
+GameState = namedtuple('State', ['table_state', 'players_state'])
+
+class State:
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def get_visible_state(self):
+        raise NotImplementedError
+
+
+class TableState(State):
+    def get_visible_state(self):
+        return self
+
+class PlayerState(State):
+    pass
+
+def create_game_state(table : TableState, players : [PlayerState]) -> GameState:
+    return GameState(table, players)
+        
+def get_visible_state(player : int, state : GameState) -> GameState:
+    table = state.table_state.get_visible_state()
+    players = [st if i == player else st.get_visible_state for i, st in enumerate(state.players_state)]
+    return create_game_state(table, players)
