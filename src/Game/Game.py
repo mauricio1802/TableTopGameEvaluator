ad66e548144321b3ac6b486e50baf48371c72b7f
Mauricio Perdomo <ml.perdomo97@gmail.com>
2020-11-06 23:00:02 -0500

    Add end_condition arg to Game constructor end_condition function is added to Game class to check when a final state is reached and compute the game final result

--- src/Game/Game.py
+++ src/Game/Game.py
@@ -2,15 +2,21 @@ from .State import State, create_game_state
 from copy import deepcopy
 
 class Game:
-    def __init__(self, flow_graph, state, players):
+    def __init__(self, flow_graph, state, players, end_condition):
         self.state_history = [ state ]
         self.flow_graph = flow_graph
         self.players = players
+        self.end_condition = end_condition
+        self.end_result = [ 0 for _ in range(len(players))]
     
     def __iter__(self):
         return self
     
     def __next__(self):
+        end_result = self.end_condition(self.current_state)
+        if end_result is not None:
+            self.end_result = end_result
+            raise StopIteration
         new_state, player_decision = self.flow_graph.reduce(deepcopy(self.current_state), self.players)
         self.state_history.append(new_state)
         self.flow_graph = self.flow_graph.go(self.current_state, player_decision)
@@ -19,7 +25,6 @@ class Game:
     @property
     def current_state(self) -> State:
         return self.state_history[-1]
-    
     
     
 
