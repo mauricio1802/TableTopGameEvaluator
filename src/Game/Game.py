8821333d04788f935e9d3b8229782f916e1d42be
Mauricio Perdomo <ml.perdomo97@gmail.com>
2020-11-13 21:47:25 -0500

    Change action method of GameDescriptor to add precedence to action and remove prev and after from GameNone

--- src/Game/Game.py
+++ src/Game/Game.py
@@ -7,22 +7,23 @@ class NoPossiblePathError(Exception):
     pass
 
 class GameNode():
-    def __init__(self, name, prev = [], after = [], default = DEFAULT_NODE_NAME):
+    def __init__(self, name, default = DEFAULT_NODE_NAME):
         self.name = name
-        self.prev = prev
-        self.after = after
         self.default = default
         self.actions = []
     
     def add_action(self, action):
         self.actions.append(action)
+    
+    def sort_actions(self):
+        self.actions = sorted(self.actions, key = lambda x : x[2]) 
 
 NoPossiblePathNode = GameNode(DEFAULT_NODE_NAME)
 
 def no_possible_path():
     raise NoPossiblePathError 
 
-NoPossiblePathNode.add_action((no_possible_path, False))
+NoPossiblePathNode.add_action((no_possible_path, False, 0))
 
 
 class Game:
@@ -43,18 +44,9 @@ class Game:
     
     def __next__(self):
         actual = self.nodes[self.actual]
-        #Execute previous actions
-        for node in actual.prev:
-            for require_play, act in self.nodes[node].actions:
-                if require_play:
-                    player = self.players[self.who_plays(deepcopy(self.actual_state), node)]
-                    self.last_play = player.get_play(deepcopy(self.actual_state))
-                    self.state_history.append(act(deepcopy(self.actual_state), self.last_play))
-                else:
-                    self.state_history.append(act(deepcopy(self.actual_state)))
         
         #Execute node actions
-        for require_play, act in actual.actions:
+        for require_play, act, _ in actual.actions:
             if require_play:
                 player = self.players[self.who_plays(deepcopy(self.actual_state), self.actual)]
                 self.last_play = player.get_play(deepcopy(self.actual_state), self.actual)
@@ -62,16 +54,6 @@ class Game:
             else:
                 self.state_history.append(act(deepcopy(self.actual_state)))
 
-        #Execute after action
-        for node in actual.after:
-            for require_play, act in self.nodes[node].actions:
-                if require_play:
-                    player = self.players[self.who_plays(deepcopy(self.actual_state), node)]
-                    self.last_play = player.get_play(deepcopy(self.actual_state))
-                    self.state_history.append(act(deepcopy(self.actual_state), self.last_play))
-                else:
-                    self.state_history.append(act(deepcopy(self.actual_state)))
-        
         #UpdateActual
         for require_play, to, cond in self.go_table[self.actual][::-1]:
             go = cond(self.actual_state, self.last_play) if require_play else cond(self.actual_state)
@@ -86,12 +68,10 @@ class Game:
         
         return self.actual_state
 
-
-
-
     @property
     def actual_state(self):
         return self.state_history[-1]
+
 
 class GameDescriptor:
     def __init__(self, start_node, nodes):
@@ -101,27 +81,32 @@ class GameDescriptor:
         self._go_table = { node.name : [(False, node.default, lambda _ : True)] for node in nodes } 
         self._end_condition = None 
         self._who_plays = None
-        
     
-    def action(self, node, require_play = False):
+    def action(self, node, require_play = False, precedence = 0):
         def f(fn):
-            self._nodes[node].add_action((require_play, fn))
+            self._nodes[node].add_action((require_play, fn, precedence))
+            return fn
         return f
     
     def goto(self, node_from, node_to, require_play = False):
         def f(fn):
             self._go_table[node_from].append((require_play, node_to, fn))
+            return fn
         return f
     
     def end(self):
         def f(fn):
             self._end_condition = fn
+            return fn
         return f
     
     def who_plays(self):
         def f(fn):
             self._who_plays = fn
+            return fn
         return f
     
     def get_game_instance(self, state, players):
-        return Game(self._start, self._nodes, self._go_table, state, players, self._who_plays, self._end_condition) 
+        for node in self._nodes.values():
+            node.sort_actions()
+        return Game(self._start, self._nodes , self._go_table, state, players, self._who_plays, self._end_condition) 
